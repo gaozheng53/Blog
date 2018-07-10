4b89b23e185312ffc740cf857b35e5013d2a09d5
Katie <gaozheng0726@gmail.com>
2018-07-10 11:12:33 -0500

    Clean code.

--- src/main/java/com/lrm/util/MarkdownUtils.java
+++ src/main/java/com/lrm/util/MarkdownUtils.java
@@ -14,9 +14,7 @@ import org.commonmark.renderer.html.HtmlRenderer;
 
 import java.util.*;
 
-/**
- * Created by limi on 2017/10/22.
- */
+
 public class MarkdownUtils {
 
     /**
@@ -75,13 +73,4 @@ public class MarkdownUtils {
     }
 
 
-    public static void main(String[] args) {
-        String table = "| hello | hi   | 哈哈哈   |\n" +
-                "| ----- | ---- | ----- |\n" +
-                "| 斯维尔多  | 士大夫  | f啊    |\n" +
-                "| 阿什顿发  | 非固定杆 | 撒阿什顿发 |\n" +
-                "\n";
-        String a = "[imCoding 爱编程](http://www.lirenmi.cn)";
-        System.out.println(markdownToHtmlExtensions(a));
-    }
 }
